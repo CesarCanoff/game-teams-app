ad7df31b3ea8e90452855b8b0e1e90c5e62f1f6f
cesarcanoff <canoff.cesar@gmail.com>
2023-07-04 23:34:13 -0400

    Implement Loading component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from "@expo-google-fonts/roboto";
+
 import { Groups } from "@screens/Groups";
 
+import theme from './src/theme';
+import { Loading } from "@components/Loading";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   );
 }
