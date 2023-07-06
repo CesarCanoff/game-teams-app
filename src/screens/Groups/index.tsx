373a131eebfaee867db22266f23989d2b5255eab
cesarcanoff <canoff.cesar@gmail.com>
2023-07-06 15:42:34 -0400

    feat: Implement Empty List component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { EmptyList } from '@components/EmptyList';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['RaseK Team']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -20,10 +21,10 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={item => item}
-        renderItem={({ item }) => (
-          <GroupCard
-            title={item}
-          />
+        renderItem={({ item }) => (<GroupCard title={item} />)}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <EmptyList message="Cadastre a primeira turma!" />
         )}
       />
 
