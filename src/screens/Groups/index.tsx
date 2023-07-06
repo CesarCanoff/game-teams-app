849c473e00b7596948fd318dfb185712a610c00e
cesarcanoff <canoff.cesar@gmail.com>
2023-07-06 15:47:24 -0400

    fix: Apply Button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { EmptyList } from '@components/EmptyList';
+import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -27,6 +28,8 @@ export function Groups() {
           <EmptyList message="Cadastre a primeira turma!" />
         )}
       />
+
+      <Button title='Criar nova turma' />
 
     </Container>
   );
