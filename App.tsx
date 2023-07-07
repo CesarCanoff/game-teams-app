9b7f18d95370ffe90cd556cfd48de34578c47d47
cesarcanoff <canoff.cesar@gmail.com>
2023-07-06 23:42:29 -0400

    feat: Implement Navigator Structure

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
+import { StatusBar } from "react-native";
+
 import { ThemeProvider } from "styled-components";
+
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold
 } from "@expo-google-fonts/roboto";
 
-import { Groups } from "@screens/Groups";
-
 import theme from './src/theme';
+import { Routes } from "./src/routes";
+
 import { Loading } from "@components/Loading";
-import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -22,7 +24,7 @@ export default function App() {
         backgroundColor="transparent"
       />
 
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
